2087311b835db8a33455805ce0e4a323f83ea7ee
Sami Kukkonen <sami@samikukkonen.fi>
2017-12-14 19:29:20 +0200

    Tweak test name

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import reducer, {setA, setB } from "./reducer";
 import reducerTest from "../src/index";
 
-describe("reducer", reducerTest(reducer)(function () {
+describe("Example reducer", reducerTest(reducer)(function () {
   this.expectInitialState({ a: "a", d: 0 });
   this.snapshotAction(setA("other a"));
   this.snapshotActionSequence(setB({ c: true }), setA("some other"));
